635d48038a5c5c94dd1f9785b20d765f0c008e9a
Guilherme Pinheiro <gui.pinheiro@outlook.com>
2022-03-12 12:10:40 -0300

    removing test files

--- Counter App/index.js
+++ Counter App/index.js
@@ -10,21 +10,30 @@ let count = 0;
 //Increment function to increase our counter as we click it
 const increment = () => {
     count++;
-    countEl.innerText = count;
+    countEl.textContent = count;
 }
 
 
 //Save functions that logs out the current counter
 let savedCounters = ""
+let savedP = document.getElementById("saved-counters")
 const save = () => {
-    savedCounters+= ` ${count} -`
-    console.log(savedCounters)
+    savedCounters = count + " - "; //`${count} - `;
+    //Here you can use the string literal that is commented in the line above
+    //with .innerText or use textContent and count + " - ". If you use the latter
+    //with .innerText, it will result in some lost blank spaces since innerText 
+    //does not handle non human readable elements (like spaces, css styling or scripts)
+    //in consideration
+    savedP.textContent += savedCounters;
+    count = 0;
+    countEl.textContent = count;
 }
 
 //Reset function to make our counter goes back to zero
 const reset = () => {
     count = 0;
-    countEl.innerText = count;
+    countEl.textContent = count;
+    savedP.textContent = "Saved:";
 }
  
 
--- Counter App/testing-js.js
+++
@@ -1,7 +0,0 @@
-let welcomeEl = document.getElementById("welcome-el");
-
-let name1 = "Guilherme";
-let greeting = "Welcome back,   ";
-
-welcomeEl.innerText = greeting + name1;
-welcomeEl.innerText += " 👋";
